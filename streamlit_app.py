938c79d4fb158e3ce0c1a9bf0de722b6e0401c7c
Meux123 <69673789+Meux123@users.noreply.github.com>
2024-10-24 11:52:43 +0000

    push

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 
 @st.cache_data
 def create_la_geojson():
-    with open('Local_Authority_Districts.geojson') as f:
+    with open('local_authority_geojson.geojson') as f:
         gj=geojson.load(f)
     example_la_data = []
     
